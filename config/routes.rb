bcf81158497dac1a9480dd22190ad4e0436b90c1
alexsummers <alexsummers@yandex.ua>
2013-09-05 21:07:43 +0300

    fix style and add pagination views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Tit::Application.routes.draw do
 
-  devise_for :users
-
   root :to => 'pictures#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  devise_for :users
 
   get 'categories/:title/:id' => 'pictures#show', as: :picture
 
