f3fffcca8d5ddb89adc6011b761150a2430e1112
alexsummers <alexsummers@yandex.ua>
2013-09-09 16:40:28 +0300

    comment small fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,4 +11,6 @@ class Comment < ActiveRecord::Base
   validates :picture_id, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :user_id, presence: true, numericality: {only_integer: true, greater_than: 0}
 
+  default_scope order('created_at DESC')
+
 end
