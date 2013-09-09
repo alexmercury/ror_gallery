3b7419d77af47f44587db0b6b5616238a019e8b6
alexsummers <alexsummers@yandex.ua>
2013-09-09 15:57:59 +0300

    fix comments & likes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,11 +6,11 @@ class PicturesController < ApplicationController
 
   def show
     @picture = Picture.includes(likes: :user, comments: :user).find(params[:id])
+    @comments = Comment.includes(:user).where('picture_id = :id', id: params[:id]).order('created_at DESC').page(params[:page])
 
     unless @picture.category.title == params[:title]
       redirect_to categories_path
     end
-
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user, counter_cache: true
   belongs_to :picture, counter_cache: true
 
+  paginates_per 5
+
   validates :description, presence: true, length: {minimum: 2, maximum: 2048}
   validates :picture_id, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :user_id, presence: true, numericality: {only_integer: true, greater_than: 0}
