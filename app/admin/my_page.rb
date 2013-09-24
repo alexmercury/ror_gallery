3083740747ad0f0180086541244b045cd23fef27
alexsummers <alexsummers@yandex.ua>
2013-09-24 19:22:15 +0300

    parse img save

--- app/admin/my_page.rb
+++ app/admin/my_page.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register_page 'My page' do
   page_action :ex, :method => :post do
     # do stuff here
 
-    parse_url = 'http://devsbs.com'
+    parse_url = 'http://im-lab.ru'
 
     doc = Nokogiri::HTML(open(parse_url))
 
@@ -14,7 +14,7 @@ ActiveAdmin.register_page 'My page' do
 
     doc.css('img').each do |image|
 
-      unless image['src'].nil? && image['src'].include?('://')
+      unless image['src'].include?('://')
 
         unless image['src'].include? parse_url.split('/')[2]
           image['src'] =  parse_url.split('/')[2] + image['src']
@@ -28,23 +28,42 @@ ActiveAdmin.register_page 'My page' do
 
     end
 
-    puts @img_src
+    @categories = Category.all
 
-    #render layout: 'active_admin'
+
     render 'admin/ex'
-    #redirect_to admin_my_page_path#, :notice => "You did stuff!#{title}"
+  end
+
+  page_action :save, :method => :post do
+
+    picture = Picture.new(params[:picture])
+
+    picture.image = open(params[:image_url])
+
+    picture.save
+
+
+    render nothing: true
   end
 
 
   content do
 
     panel 'Parse site' do
+
+      div do
+
+        form_tag(admin_my_page_ex_path(), :method => :post) do
+          url_field_tag :parse_url
+          submit_tag 'Save'
+        end
+
+      end
+
       div {link_to "Do Stuff", admin_my_page_ex_path(), :method => :post}
 
       div{ button_tag 'TTT', id: 'parse_win' }
     end
-
-
 
   end
 
--- app/assets/javascripts/active_admin.js.coffee
+++ app/assets/javascripts/active_admin.js.coffee
@@ -3,5 +3,32 @@
 $(document).ready ->
 
   $(document).on "click", "#parse_win", ->
-
     alert 'Google'
+
+  $(document).on "click", "#close_img", (event)->
+    $(this).closest('tr').remove()
+
+  $(document).on "click", "img", ->
+
+    if newImg = new Image()
+
+      newImg.src = $(this).attr 'src'
+
+      height = newImg.height
+      width = newImg.width
+
+      $('#lightbox img').attr 'src', newImg.src
+      $('#lightbox .img-box').attr 'height', (height+'px')
+      $('#lightbox .img-box').attr 'width', (width+'px')
+
+      $('#lightbox .img-box').attr 'style', ('margin-top: -'+(height/2)+'px; margin-left: -'+ (width/2)+'px;')
+
+      $('#lightbox').show()
+
+    true
+
+  $(document).on "click", "#close_lightbox", ->
+    $('#lightbox').hide()
+
+  $(document).on "click", ".overlay", ->
+    $('#lightbox').hide()
