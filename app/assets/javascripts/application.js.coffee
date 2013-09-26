c386a63b52a31b77abed0297de99c328bd498edb
alexsummers <alexsummers@yandex.ua>
2013-09-26 12:57:58 +0300

    code fix

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -2,5 +2,11 @@
 #= require jquery_ujs
 #= require likes
 #= require categories
-#= require pusher/pusher_rails
 #= require pusher
+#= require pusher/pusher_rails
+
+$(document).ready ->
+
+  pusher = new PusherRails("comments_channel")
+  pusher.channelBind.bind "comment_event", (data) ->
+    alert data.user_name + "\n" + data.comment.description
--- lib/pusher_rails.rb
+++ lib/pusher_rails.rb
@@ -2,27 +2,34 @@ require 'pusher'
 
 module PusherRails
 
-  extend self
+  class Base
 
-  Pusher.logger = Rails.logger
+    def self.initialize
+      Pusher.logger = Rails.logger
 
-  Pusher.key = Settings.pusher.app_key
-  Pusher.secret = Settings.pusher.app_secret
-  Pusher.app_id = Settings.pusher.app_id
+      Pusher.key = Settings.pusher.app_key
+      Pusher.secret = Settings.pusher.app_secret
+      Pusher.app_id = Settings.pusher.app_id
 
-  Pusher.host   = Settings.pusher.host
-  Pusher.port   = Settings.pusher.api_port
+      Pusher.host   = Settings.pusher.host
+      Pusher.port   = Settings.pusher.api_port
 
-  #Pusher.url = "http://#{Settings.pusher.app_key}:#{Settings.pusher.app_secret}@api.pusherapp.com/apps/#{Settings.pusher.app_id}"
+      #Pusher.url = "http://#{Settings.pusher.app_key}:#{Settings.pusher.app_secret}@api.pusherapp.com/apps/#{Settings.pusher.app_id}"
+      @pusher = Pusher
+    end
 
-  def trigger
-    Pusher['test_channel'].trigger('my_event', {
-        message: 'hello world'
-    })
+    def self.trigger
+      @pusher['test_channel'].trigger('my_event', {
+          message: 'hello world'
+      })
+    end
+
+    def self.comment_add(data = {})
+      @pusher['comments_channel'].trigger('comment_event', data)
+    end
+
   end
 
-  def comment_add(data = {})
-    Pusher['comments_channel'].trigger('comment_event', data)
-  end
+  Base.initialize
 
 end
