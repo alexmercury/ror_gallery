e442d807d1acb70d4170e25abcc3e6832c7ef83c
alexsummers <alexsummers@yandex.ua>
2013-10-23 11:40:29 +0300

    fix SQL query

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.includes(:users).where('slug = :slug', slug: params[:id]).first
-    @pictures = Picture.where('category_id = :id', id: @category.id).page(params[:page])
+    @pictures = Picture.includes(:category).where('category_id = :id', id: @category.id).page(params[:page])
   end
 
   def subscribe
@@ -34,7 +34,9 @@ class CategoriesController < ApplicationController
 
     category_subscribe = CategorySubscription.where('user_id = :user_id AND category_id = :category_id', user_id: current_user.id, category_id: params[:category_id]).first
 
-    if category_subscribe.destroy
+    if category_subscribe.respond_to?(:destroy)
+
+      category_subscribe.destroy
 
       Resque.enqueue(UserEvents,
                      {user_id: current_user.id,
